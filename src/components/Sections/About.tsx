ff85899e63f8e998cbd6ae2c370700596e175002
Geoffrey w <ryan5tark223232@gmail.com>
2023-06-03 21:58:11 +0800

    Revert "add chatbox"
    This reverts commit 090ea1a5a7f84e2a37fbe453c2088d61044dd7fc.

--- src/components/Sections/About.tsx
+++ src/components/Sections/About.tsx
@@ -1,46 +1,13 @@
-import { ChevronDownIcon } from '@heroicons/react/outline';
+import {ChevronDownIcon} from '@heroicons/react/outline';
 import classNames from 'classnames';
-import { FC, memo, useState } from 'react';
+import {FC, memo} from 'react';
 
-import { heroData, SectionId } from '../../data/data';
+import {heroData, SectionId} from '../../data/data';
 import Section from '../Layout/Section';
 import Socials from '../Socials';
 
 const About: FC = memo(() => {
-  const [text, setText] = useState('');
-  const [isLoading, setIsLoading] = useState(false);
-  const [response, setResponse] = useState('');
-
-  const handleSendRequest = () => {
-    setIsLoading(true);
-
-    const url = `http://34.125.239.35:7860/?prompt=${encodeURIComponent(text)}`;
-    console.log(url);
-    // Perform the GET request using the URL
-    // You can use libraries like axios or fetch to make the request
-
-    // Example using fetch:
-    fetch(url)
-      .then(response => response.json())
-      .then(data => {
-        // Handle the response data
-        console.log(data);
-        setResponse(data); // Store the response
-        setIsLoading(false);
-      })
-      .catch(error => {
-        // Handle any errors
-        console.error(error);
-        setResponse("Data Fetching Failed...");
-        setIsLoading(false);
-      });
-  };
-
-  const handleInputChange = (event: React.ChangeEvent<HTMLTextAreaElement>) => {
-    setText(event.target.value);
-  };
-
-  const { name, description, actions } = heroData;
+  const {name, description, actions} = heroData;
 
   return (
     <Section noPadding sectionId={SectionId.About}>
@@ -53,46 +20,25 @@ const About: FC = memo(() => {
               <Socials />
             </div>
             <div className="flex w-full justify-center gap-x-4">
-              {actions.map(({ href, Icon, primary, text }) => (
+              {actions.map(({href, text, primary, Icon}) => (
                 <a
                   className={classNames(
                     'flex gap-x-2 rounded-full border-2 bg-none py-2 px-4 text-sm font-medium text-white ring-offset-gray-700/80 hover:bg-gray-700/80 focus:outline-none focus:ring-2 focus:ring-offset-2 sm:text-base',
                     primary ? 'border-orange-500 ring-orange-500' : 'border-white ring-white',
                   )}
                   href={href}
-                  key={text}
-                >
+                  key={text}>
                   {text}
                   {Icon && <Icon className="h-5 w-5 text-white sm:h-6 sm:w-6" />}
                 </a>
               ))}
-            </div>
-            <div className="flex items-center w-full">
-              <textarea
-                className="bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm w-full justify-center"
-                onChange={handleInputChange}
-                placeholder="Enter your text here..."
-                rows={1}
-                value={text}
-              ></textarea>
-              <button
-                className="ml-2 bg-blue-500 hover:bg-blue-600 text-white rounded-lg px-4 py-2"
-                onClick={handleSendRequest}
-              >
-                Send
-              </button>
-            </div>
-            <div className="bg-neutral-700 border-0 focus:border-0 focus:outline-none focus:ring-1 focus:ring-orange-600 rounded-md placeholder:text-neutral-400 placeholder:text-sm text-neutral-200 text-sm w-full justify-center">
-              {isLoading && <p>Loading...</p>}
-              {!isLoading && response && <p>{response}</p>}
             </div>
           </div>
         </div>
         <div className="absolute inset-x-0 bottom-6 flex justify-center">
           <a
             className="rounded-full bg-white p-1 ring-white ring-offset-2 ring-offset-gray-700/80 focus:outline-none focus:ring-2 sm:p-2"
-            href={`/#${SectionId.Resume}`}
-          >
+            href={`/#${SectionId.Resume}`}>
             <ChevronDownIcon className="h-5 w-5 bg-transparent sm:h-6 sm:w-6" />
           </a>
         </div>
